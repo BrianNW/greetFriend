7fc21efed0511e2c7a5ee1891f4ca0780be22a94
Brian <briantechdom@gmail.com>
2018-09-12 09:58:46 -0700

    Committing challenge

---
+++ greetFriend.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string friend1 = "Frank";
+            string friend2 = "Bob";
+            string friend3 = "Tilly";
+
+
+        }
+
+        public static void GreetFriend(string friendName)
+        {
+            Console.WriteLine("Hi " + friendName + " , my friend!");
+        }
+    }
+}
